5c56a4c8059cde68c346cc400806e96678219129
neonew <neonew@gmail.com>
2015-06-16 15:06:01 +0200

    send keys using keybd_event()

--- screenshot.py
+++ screenshot.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import time
-import win32gui, win32api
+import win32gui, win32api, win32con
 from PIL import ImageGrab
 from pykeyboard import PyKeyboard
 import win32com.client
@@ -10,11 +10,21 @@ pwin = win32gui.FindWindow(None, 'Counter-Strike: Global Offensive')
 win32gui.BringWindowToTop(pwin)
 win32gui.SetForegroundWindow(pwin)
 
-# Works
-win32api.keybd_event(0x78, 0x43, 0, 0);
-time.sleep(.05)
-win32api.keybd_event(0x78, 0x43, win32con.KEYEVENTF_KEYUP, 0);
+def sendkey(vk):
+	MAPVK_VK_TO_VSC = 0
+	scancode = win32api.MapVirtualKey(vk, MAPVK_VK_TO_VSC);
+	win32api.keybd_event(vk, scancode, 0, 0);
+	time.sleep(0.01)
+	win32api.keybd_event(vk, scancode, win32con.KEYEVENTF_KEYUP, 0);
 
+def sendstring(string):
+	for c in string:
+		sendkey(ord(c.upper()))
+
+sendkey(win32con.VK_F9)
+time.sleep(0.1)
+sendkey(ord('H'))
+sendstring("Test")
 # Doesn't work
 #shell = win32com.client.Dispatch("WScript.Shell")
 #shell.SendKeys('{F9}')
