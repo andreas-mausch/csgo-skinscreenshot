da1db9c49e2ff91f8bafe3f3026948b841a975d8
neonew <neonew@gmail.com>
2015-06-16 15:14:55 +0200

    cleanup

--- screenshot.py
+++ screenshot.py
@@ -6,9 +6,10 @@ from PIL import ImageGrab
 from pykeyboard import PyKeyboard
 import win32com.client
 
-pwin = win32gui.FindWindow(None, 'Counter-Strike: Global Offensive')
-win32gui.BringWindowToTop(pwin)
-win32gui.SetForegroundWindow(pwin)
+def focusWindow(title):
+	pwin = win32gui.FindWindow(None, title)
+	win32gui.BringWindowToTop(pwin)
+	win32gui.SetForegroundWindow(pwin)
 
 def sendkey(vk):
 	MAPVK_VK_TO_VSC = 0
@@ -32,21 +33,8 @@ def execute_console_command(command):
 	time.sleep(0.1)
 	toggle_console()
 
+focusWindow("Counter-Strike: Global Offensive")
 execute_console_command("status")
-# Doesn't work
-#shell = win32com.client.Dispatch("WScript.Shell")
-#shell.SendKeys('{F9}')
-#shell.SendKeys('ashasjdo')
-
-# Doesn't work
-#keyboard = PyKeyboard()
-#keyboard.tap_key(keyboard.function_keys[9])
-#keyboard.type_string('Hello, World!')
-#keyboard.press_key('W')
-#time.sleep(2)
-#keyboard.release_key('W')
-
-time.sleep(0.2)
 
 img=ImageGrab.grab()
 img.save('Screenshot.jpg')
