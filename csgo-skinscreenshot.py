8734530bd249bf2209c978d81d9937b5c1713d38
neonew <neonew@gmail.com>
2015-06-17 13:45:28 +0200

    take screenshot from playside and backside; teleport player to same position on de_vertigo

--- csgo-skinscreenshot.py
+++ csgo-skinscreenshot.py
@@ -5,24 +5,26 @@ import pika
 import screenshot
 import time
 
-def screenshotFilename(skin):
-	return "screenshots/" + skin.replace(' ', '##') + ".png"
+def screenshotFilename(skin, view):
+	return "screenshots/" + skin.replace(' ', '##') + "###" + view + ".png"
 
 def takeScreenshot(skin):
 	csgo.focusCounterStrikeWindow()
+	csgo.executeConsoleCommand("sm_teleport 74 384 11851 20 0 0")
 	csgo.executeConsoleCommand("sm_changeskin " + skin)
-	time.sleep(5)
+	time.sleep(3)
+	screenshot.saveScreenshot(screenshotFilename(skin, "ps"))
 	csgo.sendKey(ord('F'))
 	time.sleep(2)
-	screenshot.saveScreenshot(screenshotFilename(skin))
+	screenshot.saveScreenshot(screenshotFilename(skin, "inspect"))
 
-def screenshotExists(skin):
-	return os.path.isfile(screenshotFilename(skin))
+def screenshotsExists(skin):
+	return os.path.isfile(screenshotFilename(skin, "ps")) and os.path.isfile(screenshotFilename(skin, "inspect"))
 
 def callback(ch, method, properties, body):
 	skin = body.decode("utf-8")
 	print ("Received ", skin)
-	if not screenshotExists(skin):
+	if not screenshotsExists(skin):
 		takeScreenshot(skin)
 	ch.basic_ack(delivery_tag = method.delivery_tag)
 
