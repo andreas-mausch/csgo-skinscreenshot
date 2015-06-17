709a2320711fad4eb884826ff6473b1ac945d980
neonew <neonew@gmail.com>
2015-06-17 03:58:12 +0200

    messagequeue_send: use argv parameters

--- messagequeue_send.py
+++ messagequeue_send.py
@@ -1,7 +1,10 @@
 import messagequeue
 import pika
+import sys
+
+command = ' '.join(sys.argv[1:])
 
 connection = messagequeue.open('Hauptrechner')
 channel = messagequeue.channel(connection, 'hello')
-messagequeue.send(channel, 'hello', 'messagequeue_send.py')
+messagequeue.send(channel, 'hello', command)
 connection.close()
